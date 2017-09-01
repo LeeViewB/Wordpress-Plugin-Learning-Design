8c818dd3ac8d00de1ea0f828595e6f082c9d1f03
Unknown <liviub89s@yahoo.com>
2017-09-01 19:46:30 +0300

    v 2 of Lorem Ipsum plugin generator

--- wp-content/plugins/ipsum_generator/index.php
+++ wp-content/plugins/ipsum_generator/index.php
@@ -11,25 +11,55 @@
 
 add_shortcode('ipsum','generate_ipsum');
 
-
 function generate_ipsum($atts){
     
-    if(empty($atts['paragrafe'])){
-        $paragrafe = 3;
+    if(empty($atts['type'])){
+        $type = 'text';
     }else{
-        $paragrafe = $atts['paragrafe'];
+        $type = $atts['type'];
         
-    }   
+    }  
     
+    switch($type){
+        case 'text':
+            if(empty($atts['paragrafe'])){
+                $paragrafe = 3;
+            }else{
+                $paragrafe = $atts['paragrafe'];
+
+            }   
+
+
+            if(empty($atts['lungime'])){
+                $lungime = medium;
+            }else{
+                $lungime = $atts['lungime'];
+
+            }
+
+            $ipsum = file_get_contents('http://loripsum.net/api/'.$paragrafe.'/'.$lungime);
+
+            echo $ipsum;
+            break;
     
-    if(empty($atts['lungime'])){
-        $lungime = medium;
-    }else{
-        $lungime = $atts['lungime'];
-        
+        case 'img':
+            if(empty($atts['width'])){
+                $width = 275;
+            }else{
+                $width = $atts['width'];
+
+            }
+
+
+            if(empty($atts['height'])){
+                $height = 65;
+            }else{
+                $height = $atts['height'];
+
+            }
+
+            echo "<br /><img src='http://placehold.it/".$width.'x'.$height."' />";
+
+            break;
     }
-    
-    $ipsum = file_get_contents('http://loripsum.net/api/'.$paragrafe.'/'.$lungime);
-    
-    echo $ipsum;
 }

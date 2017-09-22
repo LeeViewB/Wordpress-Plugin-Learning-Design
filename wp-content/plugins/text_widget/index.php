a5701549f86f09b566fb61eb3bdbe9e4c4d0ba92
Unknown <liviub89s@yahoo.com>
2017-09-22 19:02:53 +0300

    Coco Slider creation

--- wp-content/plugins/text_widget/index.php
+++ wp-content/plugins/text_widget/index.php
@@ -26,14 +26,39 @@ class Text_ipsum extends WP_Widget {
     }
     
     public function widget($args, $instance){
-        //ce apare aici afiseaza pe site.        
+        //ce apare aici afiseaza pe site. 
+        
+        extract($args);
+        $text = $instance['text'];
+        
+        echo $before_widget;
+        
+        echo '<p>'.$text.'</p>';
+                
+        echo $after_widget;
     }
     
     public function form($instance){
         //ce apare aici apare in Dashboard in back-end.
+        if(isset($instance['text'])){
+            $text = $instance['text'];            
+        }else{
+            $text = '';
+        }
+        
+        echo '<h2>Parerea ta conteaza, omule!</h2>';
+        
+        echo '<label>Scrie text aici:</label>';
+        echo '<input name="'.$this->get_field_name('text').'" id="'.$this->get_field_name('text').'" type="text" class="widefat" value="'.$text.'">';
+        echo '<br />';
+        echo '<br />';
     }
     
     public function update($new_instance, $old_instance){
+        // actualizeaza anumite campuri sau categorii
+        $instance = array();
         
+        $instance['text'] = (!empty($new_instance['text'])) ? strip_tags($new_instance['text']) : '' ;
+        return $instance;
     }
 }

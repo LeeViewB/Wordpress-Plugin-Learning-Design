8b7f0622664aeb981f6f21283c23863d5eeb1102
Unknown <liviub89s@yahoo.com>
2017-09-29 19:35:42 +0300

    final update

--- wp-content/plugins/coco_slider/index.php
+++ wp-content/plugins/coco_slider/index.php
@@ -11,6 +11,15 @@
 
 add_action('init','meniu_slider');
 
+add_image_size('slider_size',940,200,true);
+
+add_filter('image_size_names_choose','marimi_poze');
+function marimi_poze($sizes){
+    return array_merge($sizes, array(
+        'slider_size' => 'Marime slider',
+    ));
+}
+
 function meniu_slider() {
     $args = array(
         'label' => 'Slider',
@@ -41,13 +50,16 @@ function afisare_slider(){
     
     $slides = get_posts($args);
     
+    echo '<ul id="main_slider">';
+    
     foreach($slides as $item){
         
-        echo get_the_post_thumbnail_url($item->ID);
-        
-        echo '<br />';
-        
+        echo '<li>';
+        echo '<img src="'.get_the_post_thumbnail_url($item->ID).'">';   
+        echo '</li>';        
     }
+    
+    echo '</ul>';
     
     wp_reset_postdata();
     
@@ -60,7 +72,9 @@ function coco_style(){
 }
 
 function coco_script(){
+    wp_enqueue_script('jquery');
     wp_enqueue_script('coco-slider-js', plugin_dir_url( __FILE__ ).'js/lightslider.min.js', false);
+    wp_enqueue_script('coco-settings-js', plugin_dir_url( __FILE__ ).'js/jssettings.js', false);
 }
 
 add_action('wp_enqueue_scripts','coco_style');
---
+++ wp-content/plugins/coco_slider/js/jssettings.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+   $("#main_slider").lightSlider({
+       item:1,
+       autoWidth:true,
+       slideMove:1,
+       slideMargin:10,
+       loop:true,
+   });    
+});
